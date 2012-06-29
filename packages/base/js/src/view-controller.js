63196638205da3d4ac92213c8f3e78375681357e
Ryan Eastridge <ryan@syntacticx.com>
2012-06-28 21:49:46 -0700

    add route event handling to ViewController

--- packages/base/js/src/view-controller.js
+++ packages/base/js/src/view-controller.js
@@ -1,5 +1,20 @@
 var ViewController = Layout.extend();
-_.extend(ViewController.prototype, Router.prototype);
-ViewController.prototype.initialize = function() {
-  this._bindRoutes();
-};
+_.extend(ViewController.prototype, Router.prototype, {
+  initialize: function() {
+    Backbone.history.on('route', this._onRoute, this);
+    this.on('destroy', function() {
+      Backbone.history.off('route', this._onRoute, this);
+    });
+    this._bindRoutes();
+  },
+  _onRoute: function(router, name) {
+    if (router === this) {
+      if (this.parent) {
+        if (this.parent.getView() !== this) {
+          this.parent.setView(this);
+        }
+      }
+      this.trigger.apply(this, ['route'].concat(Array.prototype.slice.call(arguments, 1)));
+    }
+  }
+});
