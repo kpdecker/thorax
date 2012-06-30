5ccf5dd6bad89514b95f7fe4672c35b3438af9a4
Ryan Eastridge <ryan@syntacticx.com>
2012-06-30 13:10:50 -0700

    remove initBackboneLoader from init.js

--- packages/boilerplate-lumbar/js/init.js
+++ packages/boilerplate-lumbar/js/init.js
@@ -2,7 +2,6 @@ Thorax.templatePathPrefix = 'templates/';
 var Application = module.exports = Thorax.Application.create(module.exports);
 $(document).ready(function() {
   $('body').append(Application.el);
-  Application.initBackboneLoader();
   Application.start({
     pushState: false,
     root: '/'
