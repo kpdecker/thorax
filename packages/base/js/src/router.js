bba5ab239332683fbc9670840f99f3830ec58fd4
Ryan Eastridge <ryan@syntacticx.com>
2012-06-30 14:28:23 -0700

    move route and route:before event to Router

--- packages/base/js/src/router.js
+++ packages/base/js/src/router.js
@@ -1,1 +1,21 @@
-var Router = Backbone.Router.extend({});
+var Router = Backbone.Router.extend({
+  initialize: function() {
+    Backbone.history.on('route', onRoute, this);
+    this.on('destroy', function() {
+      Backbone.history.off('route', onRoute, this);
+    });
+  },
+  route: function(route, name, callback) {
+    //add a route:before event that is fired before the callback is called
+    return Backbone.Router.prototype.route.call(this, route, name, function() {
+      this.trigger.apply(this, ['route:before', name].concat(Array.prototype.slice.call(arguments)));
+      return callback.apply(this, arguments);
+    });
+  }
+});
+
+function onRoute(router, name) {
+  if (this === router) {
+    this.trigger.apply(this, ['route'].concat(Array.prototype.slice.call(arguments, 1)));
+  }
+}
--- packages/base/js/src/view-controller.js
+++ packages/base/js/src/view-controller.js
@@ -1,32 +1,16 @@
 var ViewController = Layout.extend();
 _.extend(ViewController.prototype, Router.prototype, {
   initialize: function() {
-    this.on('route:before', this._onBeforeRoute, this);
-    Backbone.history.on('route', this._onRoute, this);
-    this.on('destroy', function() {
-      Backbone.history.off('route', this._onRoute, this);
-    });
-    this._bindRoutes();
-  },
-  route: function(route, name, callback) {
-    //wrap the route callback (route event is too late)
-    //and set the view controller as the view on the
-    //parent if parent was set
-    return Router.prototype.route.call(this, route, name, function() {
-      this.trigger.apply(this, ['route:before', name].concat(Array.prototype.slice.call(arguments)));
-      return callback.apply(this, arguments);
-    });
-  },
-  _onBeforeRoute: function(router, name) {
-    if (this.parent) {
-      if (this.parent.getView() !== this) {
-        this.parent.setView(this);
+    Router.prototype.initialize.call(this);
+    //set the ViewController as the view on the parent
+    //if a parent was specified
+    this.on('route:before', function(router, name) {
+      if (this.parent) {
+        if (this.parent.getView() !== this) {
+          this.parent.setView(this);
+        }
       }
-    }
-  },
-  _onRoute: function(router, name) {
-    if (this === router) {
-      this.trigger.apply(this, ['route'].concat(Array.prototype.slice.call(arguments, 1)));
-    }
+    }, this);
+    this._bindRoutes();
   }
 });
